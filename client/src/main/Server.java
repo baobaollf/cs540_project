fb49d97977da94f54d60f10f5cc658b6db703884
Claudio Jimenez <clausji@mac.com>
2021-05-02 16:13:18 -0500

    added server test to client

--- client/src/main/Server.java
+++ client/src/main/Server.java
@@ -11,8 +11,15 @@ public class Server {
     String IP;
     int port;
 
-    Server(String IP, int port) {
+    public Server(String IP, int port) {
         this.IP = IP;
         this.port = port;
+    }
+    public String getIP() {
+        return IP;
+    }
+
+    public int getPort() {
+        return port;
     }
 }
